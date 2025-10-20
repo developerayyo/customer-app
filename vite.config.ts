6c31fa918061e3798dc92f495c5b537ab8d2c173
Peter Babalola <ayodelebabalola1004@gmail.com>
2025-10-20 12:02:15 +0100

    fix: pwa manifest and vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'service-worker.ts',
+      injectManifest: {
+        rollupFormat: 'iife'
+      },
+      manifest: false,
+      devOptions: { enabled: false }
+    })
+  ],
   server: {
     // https: true,
     host: true,
